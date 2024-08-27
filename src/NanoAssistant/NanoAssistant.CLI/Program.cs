217d294d9f188b68d17ebac1ed6ae12e6b2ebea5
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-08-28 00:23:13 +0800

    first commit

---
+++ src/NanoAssistant/NanoAssistant.CLI/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using NanoAssistant.Core.SemanticPlugins;
+
+Console.WriteLine("Hello, World!");
+
+var configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddUserSecrets<Program>();
+
+var configuration =configurationBuilder.Build();
+
+var builder = Kernel.CreateBuilder();
+
+// Add builder configuration and services
+builder.AddOpenAIChatCompletion("gpt-3.5-turbo-0125", configuration["OpenAPIKey"]);
+builder.Plugins.AddFromType<FinanceTrackerPlugin>("FinanceTracker");
+
+var kernel = builder.Build();
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+ChatHistory chatHistory = [];
+chatHistory.AddSystemMessage("You are a friendly personal assitant named Nano assistant.");
+
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+while (true)
+{
+    string input = Console.ReadLine();
+    chatHistory.AddUserMessage(input);
+    var result = await chat.GetChatMessageContentAsync(chatHistory, executionSettings: openAIPromptExecutionSettings, kernel: kernel);
+    chatHistory.Add(result);
+    Console.WriteLine($"assistant: {result}");
+}
---
+++ src/NanoAssistant/NanoAssistant.Core/SemanticPlugins/FinanceTrackerPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanoAssistant.Core.SemanticPlugins
+{
+    public class FinanceTrackerPlugin
+    {
+        private static int _balance = 0;
+        private static int _totalExpense = 0;
+        private static int _totalIncome = 0;
+
+        [KernelFunction("get_balance")]
+        [Description("Get the total balance in cents.")]
+        public async Task<int> GetBalance()
+        {
+            return _balance;
+        }
+
+        [KernelFunction("add_expense")]
+        [Description("Add the expense in cents to balance and returns the total expense in cents.")]
+        public async Task<int> AddExpense(int expense)
+        {
+            _totalExpense += expense;
+            _balance -= expense;
+            return _totalExpense;
+        }
+
+        [KernelFunction("add_income")]
+        [Description("Add the income in cents to balance and returns the total income in cents.")]
+        public async Task<int> AddIncome(int income)
+        {
+            _totalIncome += income;
+            _balance += income;
+            return _totalIncome;
+        }
+    }
+}
