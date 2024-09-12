7c5aa3afc7f42f601ec9e61ccd9e92bb6a9ffab1
Vicente Albonian <Vicente.Albonian@ticketek.com.au>
2024-09-12 14:26:58 +0800

    change dollar display

--- src/NanoAssistant/NanoAssistant.Core/Grains/NanoAssistantGrain.cs
+++ src/NanoAssistant/NanoAssistant.Core/Grains/NanoAssistantGrain.cs
@@ -37,7 +37,7 @@ namespace NanoAssistant.Core.Grains
         {
             State.ChatHistory.AddSystemMessage("You are a personal assistant named Nano assistant.");
             State.ChatHistory.AddSystemMessage($"Today is {DateTimeOffset.Now.ToString("d")}.");
-            State.ChatHistory.AddSystemMessage("Use dollars when dealing with money.");
+            State.ChatHistory.AddSystemMessage("Please display money in it's dollar value.");
             State.ChatHistory.AddSystemMessage("When asking for balance return the financial summary in markdown.");
         }
 
